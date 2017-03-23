994406fcedd70e119ab1644cf342aa0e37d6eed2
Gerlach, Andreas <Andreas.gerlach@dieboldnixdorf.com>
2017-03-23 16:21:08 +0100

    remove scaffolded routing code

--- services/RouteHandler.jsx
+++ services/RouteHandler.jsx
@@ -1,9 +1,5 @@
 // @flow
 
-import MainLayout from '../views/MainLayout'
-import Home from '../views/Home'
-import SettingsManager from '../views/SettingsManager'
-
 /**
  * Manages and maintains all routes internal to the
  * application. Allows extensions to register new routes on their behalf.
@@ -19,21 +15,12 @@ class RouteHandler {
   constructor (appConfig, extensionManager) {
     this.config = appConfig
     this.extensionManager = extensionManager
-    this.routeMap = {
-      path: '/',
-      component: MainLayout,
-      indexRoute: { component: Home },
-      childRoutes: [{
-        path: 'settings',
-        component: SettingsManager
-      }]
-    }
+    this.routeMap = {  }
   }
 
   get routes () {
     return this.routeMap
   }
-
 }
 
 export default RouteHandler
